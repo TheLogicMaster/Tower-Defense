3e62f30e9a602fd058df64e6dcdb5d11e3c0cebc
Dactaxx <dmpribak@gmail.com>
2015-12-09 16:54:37 -0500

    test

--- towerDefense/Window.java
+++ towerDefense/Window.java
@@ -1,6 +1,5 @@
 package towerDefense;
 
-<<<<<<< HEAD
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
@@ -26,30 +25,4 @@ public class Window {
 		
 	}
 	
-=======
-import javax.swing.JFrame;
-
-public class Window extends JFrame{
-
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	public Window(){
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);//end program on close
-		this.setExtendedState(MAXIMIZED_BOTH);//full screen
-		this.setVisible(true);
-		this.setName("Tower Defense Game");//what will we call it?
-		this.setResizable(false);
-		this.setEnabled(true);
-		//this.getContentPane() is the panel everything is on
-		//put a panel for buttons and stuff and a panel for the map on there. We can also layer panels, too.
-	}
-
-	public void repaint(){//called on refresh
-		
-	}
->>>>>>> a05b423cd5c37f1cd38f0c58b42fe74659492f2e
 }
