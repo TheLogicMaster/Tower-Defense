6075da151fe2852f938164f1f07ce8d494128c18
justinmarentette11 <justinmarentette11@gmail.com>
2015-12-09 22:26:58 -0500

    Used ALL the memory

--- towerDefense/Main.java
+++ towerDefense/Main.java
@@ -1,9 +1,18 @@
 package towerDefense;
 
 public class Main {
+	public static double px[] = new double[999999999];
+	public static double py[] = new double[999999999];
+	public static double curves = 4;
+	public static double res = 10;
 
 	public static void main(String[] args) {
+		towerDefense.map.tracks.TrackOneInOut.trackGen();
+		for (int i = 0; i<=curves;i++){
+			System.out.println("px:" + (px[i]) + " py:" + (py[i]));
+		}
 		Window w = new Window();
+
 	}
 
 }
